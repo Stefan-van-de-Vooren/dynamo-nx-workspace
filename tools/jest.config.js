7be6a860a3d45bd4e2371dacdd7084ee3925703e
Daniel Pecos Martinez <Daniel.Pecos.Martinez@rabobank.nl>
2018-10-30 13:51:01 +0100

    remove schematics as now are a dependency

--- tools/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    roots: [ 'schematics' ],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest'
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ]
-};
